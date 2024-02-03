a116daae06cea6b7e1c19ce0ce93c418472e21f7
elias-loew <125573430+elias-loew@users.noreply.github.com>
2024-02-03 19:52:30 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
+let text_list: string[] = []
+let list: Image = null
 basic.forever(function () {
-	
+    let sprite: game.LedSprite = null
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    led.plotBarGraph(
+    1,
+    10
+    )
+    null.delete()
+    sprite.turn(Direction.Right, 45)
+    game.pause()
+    sprite.set(LedSpriteProperty.X, 0)
+    text_list = ["a", "b", "c"]
+    list = images.createBigImage(`
+        # # # # # # # # # #
+        # # # # # # # # # #
+        # # # # # # # # # #
+        # # # # # # # # # #
+        # # # # # # # # # #
+        `)
 })
